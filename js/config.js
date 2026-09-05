036ff8d5658583e72ec71e0e48475b73e61a72d8
sortcup <cisoeprhddooemeb@gmail.com>
2026-09-05 15:32:02 +0200

    klyvo.tn

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 export const STORE_CONFIG = Object.freeze({
-  name: 'TechZone Tunisie',
+  name: 'klyvo.tn',
   apiUrl: 'https://script.google.com/macros/s/AKfycbyjUcRW_Nd4yOgr_s1oFF-Q6WwnPqVrl2CsjtCN0AxbVeg6lIDU0NFWexLguEa-beTz/exec',
   deliveryFee: 8,
   freeDeliveryThreshold: 250,
-  email: 'contact@techzone.tn',
+  email: 'contact@klyvo.tn',
   phone: '+216 70 000 000',
   address: 'Tunis, Tunisie',
   cacheDurationMs: 5 * 60 * 1000,
