703514374cde9a40ffdf975f539e5a100ca3fcdc
sortcup <cisoeprhddooemeb@gmail.com>
2026-09-06 18:52:20 +0200

    config

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 export const STORE_CONFIG = Object.freeze({
   name: 'klyvo.tn',
-  apiUrl: 'https://script.google.com/macros/s/AKfycbzSFik0DnIwRqKBOg9iht0xzoQib4YHautfyjonL-kcm6P44HvpN4yG7r1wTvsMWzib/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbymRk6YtwV0hxvO1-Edfwfg3Xtx9pZZExN9tU2A2N7fIunSbLNnblxjNP0DTFjP4b3U/exec',
   deliveryFee: 8,
   freeDeliveryThreshold: 250,
   email: 'contact@klyvo.tn',
   phone: '+216 27297555',
   address: 'sfax, Tunisie',
   cacheDurationMs: 5 * 60 * 1000,
-  
+
 });
