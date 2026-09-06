f189dcf40349fac1c5c571cf373f51f7ac3e540a
sortcup <cisoeprhddooemeb@gmail.com>
2026-09-06 17:30:32 +0200

    config api

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const STORE_CONFIG = Object.freeze({
   name: 'klyvo.tn',
-  apiUrl: 'https://script.google.com/macros/s/AKfycbyDVeIpxiqpNyde44u6U0gSR8UVvHy6CBHAbEJxbJU-s1ZGxk3JLrRGpMLcLMhEQ7ua/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzSFik0DnIwRqKBOg9iht0xzoQib4YHautfyjonL-kcm6P44HvpN4yG7r1wTvsMWzib/exec',
   deliveryFee: 8,
   freeDeliveryThreshold: 250,
   email: 'contact@klyvo.tn',
