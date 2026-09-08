5954d15c9a257f9d3e91380b00cb092e6b18bb11
sortcup <cisoeprhddooemeb@gmail.com>
2026-09-08 11:18:55 +0200

    num tel

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ export const STORE_CONFIG = Object.freeze({
   deliveryFee: 8,
   freeDeliveryThreshold: 250,
   email: 'contact@klyvo.tn',
-  phone: '+216 27297555',
+  phone: '+216 22592448',
   address: 'sfax, Tunisie',
   cacheDurationMs: 5 * 60 * 1000,
 
